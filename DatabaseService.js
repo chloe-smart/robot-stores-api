df31c640ac8606e23fe97fab3aaa2addda05e876
Chloe Smart <121047441+chloe-smart@users.noreply.github.com>
2023-05-17 18:13:37 +0100

    Update DatabaseService.js

--- DatabaseService.js
+++ DatabaseService.js
@@ -4,7 +4,7 @@ async function connectToDb() {
     const mongoUrl = 'mongodb://root:password@localhost:27017'
 
     const connection = await MongoClient.connect(mongoUrl)
-	const collection = connection.db('robot-stores-api').collection('products.json')
+    const collection = connection.db('robot-stores-api').collection('products.json')
     return collection
 }
 
